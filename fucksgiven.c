fe0668e99bd98b236764aba5b5b3861497d0b562
Debian <Fahrradkette@users.noreply.github.com>
2015-02-20 00:36:36 +0100

    Implement printing in fucksgiven.c
    Not all error checking done yet

--- fucksgiven.c
+++ fucksgiven.c
@@ -7,9 +7,36 @@ else print 0
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
-int main(int argc, char **argv)
+#define STOREDFUCKS "/tmp/accumulatedfucks"
+
+void get_fucks(FILE *fp, unsigned long *fucks)
 {
+    rewind(fp);
+    if (fread(fucks, sizeof(*fucks), 1, fp) == 1)
+    {
+        return;
+    }
+    *fucks = 0;
+}
+
+int main(void)
+{
+    unsigned long fucks;
+    FILE *fp;
+    fp = fopen(STOREDFUCKS, "r+b");
+    if (fp == NULL)
+    {
+        printf("Zero fucks given!");
+        return EXIT_SUCCESS;
+    }
+    get_fucks(fp, &fucks);
+    switch (fucks)
+    {
+        case 0: printf("Zero fucks given!\n"); break;
+        case 1: printf("One single fuck given!\n"); break;
+        default : printf("%d fucks given!\n", fucks);
+    }
+    fclose(fp);
     return EXIT_SUCCESS;
 }
