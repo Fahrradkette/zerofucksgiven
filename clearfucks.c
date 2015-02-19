88fe652311814674dd78a4502ceb0bdf9d5419e4
Debian <Fahrradkette@users.noreply.github.com>
2015-02-20 00:19:59 +0100

    Implement resetting functionality to clearfucks.
    Still kinda verbose, not all error checking done yet

--- clearfucks.c
+++ clearfucks.c
@@ -6,9 +6,53 @@ write 0
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
-int main(int argc, char **argv)
+#define STOREDFUCKS "/tmp/accumulatedfucks"
+
+void get_fucks(FILE *fp, unsigned long *fucks)
 {
+    rewind(fp);
+    if (fread(fucks, sizeof(*fucks), 1, fp) == 1)
+    {
+        //printf("read returns %d\n", *fucks);
+        return;
+    }
+    {
+        *fucks = 0;
+    }
+}
+
+void store_fucks(FILE *fp, unsigned long *fucks)
+{
+    rewind(fp);
+    if (fwrite(fucks, sizeof(*fucks), 1, fp) != 1)
+    {
+        printf("writing error\n");
+        return;
+    }
+    //printf("writing %d to file\n", *fucks);
+}
+
+int main(void)
+{
+    unsigned long fucks;
+    FILE *fp;
+    fp = fopen(STOREDFUCKS, "r+b");
+    if (fp == NULL)
+    {
+        printf("File \"%s\" doesn't exist, creating it.\n", STOREDFUCKS);
+        fp = fopen(STOREDFUCKS, "w+b");
+    }
+    get_fucks(fp, &fucks);
+    if (fucks == 0)
+    {
+        printf("0 fucks given so far!\n");
+        fclose(fp);
+        return EXIT_SUCCESS;
+    }
+    printf("Clearing %d fucks.\n", fucks);
+    fucks = 0;
+    store_fucks(fp, &fucks);
+    fclose(fp);
     return EXIT_SUCCESS;
 }
